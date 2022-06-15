088a62d061a4868510fb2c5352bd118601ae4aed
yongseok-web <yongseok.web@gmail.com>
2022-06-15 11:19:47 +0900

    hello commit

---
+++ Chapter/CS101_01_01.py
@@ -0,0 +1,20 @@
+"""
+ * Python_CS101_CHAPTER01
+ *
+ * @author soaring(yongseok.dev@gmail.com)
+ *
+ * 2022.06.15.
+"""
+"""
+ * Python 프로그램 작성 예제
+"""
+
+def print_message():
+    print("cs101 is fantastic!")
+    print("Programming is so much fun!")
+
+def repeat_message():
+    print_message()
+    print_message()
+
+repeat_message()
---
+++ Chapter/CS101_01_02.py
@@ -0,0 +1,10 @@
+"""
+ * Python_CS101_CHAPTER01
+ *
+ * @author soaring(yongseok.dev@gmail.com)
+ *
+ * 2022.06.15.
+"""
+"""
+ * Python 프로그램 작성 예제 함수의 활용
+"""
