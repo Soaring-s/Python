090eca9196a0a4681f248a56f8e940eab43af67f
yongseok-web <yongseok.web@gmail.com>
2022-06-15 12:10:18 +0900

    end

--- Chapter/CS101_01_02.py
+++ Chapter/CS101_01_02.py
@@ -6,5 +6,28 @@
  * 2022.06.15.
 """
 """
- * Python 프로그램 작성 예제 함수의 활용
+ * Python 프로그램 작성 예제 hubo를 사용한 함수의 활용
 """
+# import_library
+from ast import For
+from cs1robots import *
+
+#opne
+create_world()
+
+#create_hubo
+hubo = Robot(beepers = 1)
+
+# basic function
+hubo.move()
+hubo.turn_left()
+
+# turn_right()
+def turn_right():
+    hubo.turn_left()
+    hubo.turn_left()
+    hubo.turn_left()
+
+# for 
+for i in range(4):
+    print("CS101 is factastic!")
